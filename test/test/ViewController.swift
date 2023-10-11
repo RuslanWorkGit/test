40e4c96420329b629c224ea3510142524c57703f
RuslanWorkGit <lulka758@gmail.com>
2023-10-11 12:00:32 +0300

    Third commit

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let rec = CGRect(x: 50, y: 50, width: 200, height: 200)
+        var simpleView = UIView(frame: rec)
+        simpleView.backgroundColor = .magenta
+        view.addSubview(simpleView)
     }
 
     //My first change
+    
+    //My second changes
 
 }
 
